9008ea020bd1d8d0b7f68ebc021d7dedc614f437
pudongping <276558492@qq.com>
2022-10-28 01:17:29 +0800

    Create time.go

---
+++ timex/time.go
@@ -0,0 +1,50 @@
+package timex
+
+import (
+	"strings"
+	"time"
+)
+
+// Date 格式化时间或者日期
+// format := "Y-m-d H:i:s"
+// output: 2022-10-28 00:42:09
+func Date(format string, ts ...time.Time) string {
+	patterns := []string{
+		// 年
+		"Y", "2006", // 4 位数字完整表示的年份
+		"y", "06", // 2 位数字表示的年份
+
+		// 月
+		"m", "01", // 数字表示的月份，有前导零
+		"n", "1", // 数字表示的月份，没有前导零
+		"M", "Jan", // 三个字母缩写表示的月份
+		"F", "January", // 月份，完整的文本格式，例如 January 或者 March
+
+		// 日
+		"d", "02", // 月份中的第几天，有前导零的 2 位数字
+		"j", "2", // 月份中的第几天，没有前导零
+
+		"D", "Mon", // 星期几，文本表示，3 个字母
+		"l", "Monday", // 星期几，完整的文本格式;L的小写字母
+
+		// 时间
+		"g", "3", // 小时，12 小时格式，没有前导零
+		"G", "15", // 小时，24 小时格式，没有前导零
+		"h", "03", // 小时，12 小时格式，有前导零
+		"H", "15", // 小时，24 小时格式，有前导零
+
+		"a", "pm", // 小写的上午和下午值
+		"A", "PM", // 小写的上午和下午值
+
+		"i", "04", // 有前导零的分钟数
+		"s", "05", // 秒数，有前导零
+	}
+	replacer := strings.NewReplacer(patterns...)
+	format = replacer.Replace(format)
+
+	t := time.Now()
+	if len(ts) > 0 {
+		t = ts[0]
+	}
+	return t.Format(format)
+}
