bb4e694840a3fd65c0028f80dbfa2e298678cac4
pudongping <276558492@qq.com>
2022-10-28 17:03:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/pudongping/go-function-helpers/arrayx"
+	"github.com/pudongping/go-function-helpers/console"
 )
 
 func main() {
-	i := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	size := 3
-
-	r1 := arrayx.ArrayChunk(i, size)
-	fmt.Println(r1)
-
-	r1[2][1] = 100
-	fmt.Println(r1)
-
-	fmt.Println(i)
+	s := "hello world"
+	console.Success(s)
+	console.Danger(s)
+	console.Warning(s)
+	console.Info(s)
 }
